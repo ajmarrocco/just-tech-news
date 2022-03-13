ca7e397111f382b377261c9e28f388c54218fadd
ajmarrocco <ajmarrocco@gmail.com>
2022-03-13 01:02:49 -0500

    add path

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -7,9 +8,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets
+// DO NOT put under app.use(routes)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
 
 // turn on connection to db and server.  
 // Force = true (we will make the tables re-create if there are any association changes)

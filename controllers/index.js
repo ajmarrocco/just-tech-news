422f6ff2d2294947924a1cddd6fe1112359fc0c1
ajmarrocco <ajmarrocco@gmail.com>
2022-03-14 20:21:33 -0400

    add dashboard to navigation

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 // Collects packaged API routes endpoints
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 
 //prefixes userRoutes with the path /users. Takes the routes and implement them to another router instance 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
